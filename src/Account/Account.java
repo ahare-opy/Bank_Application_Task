bcd2bf14ec3201c09d6c38659a34431ea0b7b6f8
TanvirYeasinOpy <tanvir016opy@gmail.com>
2024-01-30 13:35:03 +0600

    Created the Account class, which requires the name of the account holder, account number, initial deposit amount and the minimum balance according to the account type.

---
+++ src/Account/Account.java
@@ -0,0 +1,37 @@
+package Account;
+
+import java.time.LocalDate;
+
+public class Account {
+    protected String name;
+    protected int accountNumber;
+    protected LocalDate creationDate;
+    protected double balance;
+    protected double minimumBalance;
+
+    public Account(String name, int accountNumber, double initialDeposit, double minimumBalance) {
+        this.name = name;
+        this.accountNumber = accountNumber;
+        this.balance = initialDeposit;
+        this.minimumBalance = minimumBalance;
+    }
+
+    public void deposit(double deposit) {
+        balance += deposit;
+    }
+
+    public boolean withdraw(double amount) {
+        if(balance - amount < minimumBalance) return false; //In case an user crosses the minimum balance for the account.
+
+        balance -= amount;
+        return true;
+    } 
+
+    public String getDetails() {
+        return "Account Number: " + accountNumber + "\nName: " + name + "\nBalance: " + balance + "\n";
+    }
+
+    public int getAccountNumber() { //Will be used to identify an account.
+        return accountNumber;
+    }
+}
